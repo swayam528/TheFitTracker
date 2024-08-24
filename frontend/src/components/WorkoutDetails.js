c800592b8720a0212a397db059250593b2fc7bd0
swayam528 <107274953+swayam528@users.noreply.github.com>
2024-08-24 11:04:35 -0500

    Update WorkoutDetails.js

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -13,7 +13,7 @@ const WorkoutDetails = ({ workout }) => {
       return
     }
 
-    const response = await fetch('/api/workouts/' + workout._id, {
+    const response = await fetch('https://fitnesstrack-gk1s.onrender.com/api/workouts/' + workout._id, {
       method: 'DELETE',
       headers: {
         'Authorization': `Bearer ${user.token}`
